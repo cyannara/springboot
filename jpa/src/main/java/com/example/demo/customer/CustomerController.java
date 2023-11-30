a469e86bc87a81344afade5022790f695d13f830
chichi <cyannara@naver.com>
2023-11-30 12:49:35 +0900

    jpa 프로젝트에서 storm 기능 제거

--- jpa/src/main/java/com/example/demo/customer/CustomerController.java
+++ jpa/src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,17 +1,26 @@
 package com.example.demo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@RestController  //Controller, ResponseBody
+@Controller  //Controller, ResponseBody
 public class CustomerController {
 
 	@Autowired CustomerRepository repo;
 	
-	@GetMapping("/customer")
-	public Iterable<Customer> findall(){
+	@ResponseBody
+	@GetMapping("/findcustomer")
+	public Iterable<Customer> findcustomer(){
 		return repo.findAll();
+	}
+	
+	@GetMapping("/customer")
+	public String findall(Model model){
+		model.addAttribute("customer", repo.findAll());
+		return "customer";
 	}
 }
