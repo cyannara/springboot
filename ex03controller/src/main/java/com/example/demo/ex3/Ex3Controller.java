f3bd8d82a93f4f86abb82db942fca1aed9a25b6f
cyannara <cyannara@naver.com>
2025-09-02 08:18:59 +0900

    valid

--- ex03controller/src/main/java/com/example/demo/ex3/Ex3Controller.java
+++ ex03controller/src/main/java/com/example/demo/ex3/Ex3Controller.java
@@ -14,6 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+/*
+ * 1. @Valid는 자바 표준 스펙이고 @Validated는 스프링에서 제공하는 어노테이션이다.
+   2. @Validated를 통해 그룹 유효성 검사나 Controller가 아닌 다른 계층에서 유효성 검증 가능
+   3. @Valid는 MethodArgumentNotValidException 예외를 @Validated는 ConstraintViolationException 예외를 발생시킨다.
+   4. Valid 어노테이션은 주로 request body를 검증하는데 많이 사용
+ */
+
 //@Log4j2
 @Controller
 public class Ex3Controller {
